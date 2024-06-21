3c52d54d180e0a2f5a9bee280fc252c569e005cb
Michael Dippel <michaeldippel0430@gmail.com>
2024-06-21 14:44:08 +0200

    fix some bugs and make the code better formated

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from time import sleep
 
+import sqlite3
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from pprint import pprint
 import sys
 import argparse
 import logging
-
+#spotify authentication
 scope = "user-top-read,user-read-playback-state,user-modify-playback-state,playlist-modify-private,playlist-modify-public,user-read-recently-played,user-library-read,user-library-modify"
-ranges = ["short_term", "medium_term", "long_term"]
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
 username = "dfd0430"
 
-skiplist = []
-preliminary_skiplist = []
-addlist = []
-put_on = []
 
 
 def get_recently_played_names():
@@ -101,11 +95,12 @@ def check_skip(
         current_song_progress,
         current_song_name,
         song_duration,
-        song_changed
+        song_changed,
+
 
 ):
 
-    if not check_album():
+    if not check_album() :
         if song_changed:
             convert_to_skiplist()
             if song_from_queue(old_queue_names, current_song_name) and resuming():
@@ -113,7 +108,7 @@ def check_skip(
                 while old_queue_names[i] != current_song_name:
                     add_skip(old_queue_names[i])
                     i+=1
-        if (current_song_progress) > (song_duration * 0.7):
+        if current_song_progress > (song_duration * 0.7):
             remove_preliminary_skip(current_song_name)
         else:
             add_preliminary_skip(current_song_name)
@@ -126,15 +121,7 @@ def check_in_queue(old_queue_names, current_queue_names):
             if current_queue_names[i] not in old_queue_names and i < 10:
                 addlist.append(current_queue_names[i])
 
-def check(current_recently_played_names, most_recent_song):
-    for songs in preliminary_skiplist:
-        if songs in current_recently_played_names[:5]:
-            preliminary_skiplist.remove(songs)
-        elif most_recent_song != current_recently_played_names[0] and most_recent_song not in preliminary_skiplist:
-            skiplist.append(preliminary_skiplist[0])
-            preliminary_skiplist.remove(songs)
 
-    return current_recently_played_names[0]
 
 
 def get_timestamp_current():
@@ -147,68 +134,60 @@ def get_finished():
 
 def check_put_on(old_queue_names, current_queue_names, current_song_name):
     if not check_same_queue(old_queue_names,
-                            current_queue_names) and current_song_name not in old_queue_names and current_song_name:
+                            current_queue_names) and current_song_name not in old_queue_names and song_changed:
         put_on.append(current_song_name)
 
+
+skiplist = []
+preliminary_skiplist = []
+addlist = []
+put_on = []
 
 old_queue_names = []
 song_before=""
 last_song_name = ""
 old_recently_played_names = []
 count = 0
-most_recent_song = ""
 song_changed = False
+
 while True:
+    #get info current state
     current_queue_names = get_queue_names()
     current_song_name = sp.current_playback()["item"]["name"]
     current_recently_played_names = get_recently_played_names()
+
     if last_song_name != current_song_name:
         song_changed = True
     else:
         song_changed = False
 
-
-
-
+    #check for skips and such
     check_skip(
         sp.current_playback()["progress_ms"],
         current_song_name,
         sp.current_playback()["item"]["duration_ms"],
-        song_changed
+        song_changed,
     )
-    print(preliminary_skiplist)
-    print(skiplist)
-    # pprint(sp.current_playback())
-    # print(sp.current_playback()["actions"]["disallows"])
-    # pprint(sp.queue())
-    # print(check_album())
-    # print(get_timestamp_current())
-    # print(get_finished())
+    # if song_before == "":
+    #     song_before = current_song_name
+    # elif song_changed:
+    #     song_before =last_song_name
 
-    # print("current[0]:")
-    # print(current_recently_played_names[0])
-    # print("skiplist:")
-    # print(skiplist)
-    # # print("preliminary skiplist:")
-    # # print(preliminary_skiplist)
-    # # print("current song "+current_song_name)
-    # print("current")
-    # print(current_queue_names)
-    # print("old")
-    # print(old_queue_names)
+    check_put_on(old_queue_names, current_queue_names, current_song_name)
+    check_in_queue(old_queue_names, current_queue_names)
+
+    old_queue_names = current_queue_names
+    last_song_name = current_song_name
+    old_recently_played_names = current_recently_played_names
+
+    print("skiplist")
+    print(skiplist)
     print("addlist:")
     print(addlist)
     print("put on:")
     print(put_on)
-    #
-    # print(count)
-    count += 1
-    check_put_on(old_queue_names, current_queue_names, current_song_name)
-    check_in_queue(old_queue_names, current_queue_names)
-    old_queue_names = current_queue_names
-    last_song_name = current_song_name
-    old_recently_played_names = current_recently_played_names
     print("-----------------------------------------------")
-    #x = check(current_recently_played_names, most_recent_song)
-    most_recent_song = current_recently_played_names[0]
+
+
+
     sleep(5)
